ff19fb4e061047aa0393a73563d9fa5db0a961cf
Bryan Thornbury <bt.mozoby@gmail.com>
2017-05-12 02:12:57 -0700

    first attempt add video

--- Pod/Classes/Controller/PhotosViewController.swift
+++ Pod/Classes/Controller/PhotosViewController.swift
@@ -302,7 +302,7 @@ final class PhotosViewController : UICollectionViewController {
         fetchOptions.sortDescriptors = [
             NSSortDescriptor(key: "creationDate", ascending: false)
         ]
-        fetchOptions.predicate = NSPredicate(format: "mediaType = %d", PHAssetMediaType.image.rawValue)
+        fetchOptions.predicate = NSPredicate(format: "mediaType = %d || mediaType = %d", PHAssetMediaType.image.rawValue, PHAssetMediaType.video.rawValue)
         initializePhotosDataSourceWithFetchResult(PHAsset.fetchAssets(in: album, options: fetchOptions), selections: selections)
     }
     
